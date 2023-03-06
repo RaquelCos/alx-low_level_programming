b9e5a8922c122b0d65250b7bfe616eb765562e56
Your Name <you@example.com>
2023-03-06 07:21:51 -0800

    First cTask

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * Task5 - 0x0-hello_world
+ * prints: with proper grammar, but the outcome is a piece of art, and new line 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
