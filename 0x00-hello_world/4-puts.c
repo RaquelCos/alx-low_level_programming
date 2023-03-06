b0a44ebc50544ef6fa2083e79d5e79f61580622c
Your Name <you@example.com>
2023-03-06 14:15:28 -0800

    First commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry 
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0 (Sucess)
  */
- int main(void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
