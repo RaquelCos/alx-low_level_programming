cc8e62dae8062eb9a3a7d6f4e7bf71a112f92aca
RaquelCos <you@example.com>
2023-06-07 06:34:12 -0700

    Third Task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - functions that calculates the length of the string
+ * @s: string being calculates for its length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1);
+}
