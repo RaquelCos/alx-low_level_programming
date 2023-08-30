e4e7a725d5b6eb5c14da14ced1ae62c7c1bd4ade
Raquel Costelha <raquelcostelha@gmail.com>
2023-08-30 04:13:45 -0700

    Update

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dlistint_s - Doubly linked list
+ * @n: Int
+ * @prev: Points to previous node
+ * @next: Points to next node
+ *
+ * Description: Doubly linked list node for Doubly C Project
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+}dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
