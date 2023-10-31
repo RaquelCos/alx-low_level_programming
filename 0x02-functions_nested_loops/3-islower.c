21a66cacf017da1bc18393dbd306d4574a1bcff2
RaquelCos <raquelcostelha@gmail.com>
2023-10-31 08:11:57 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - Cecks for lowercase
+ * @c: Character checked
+ * Return: 1 (lowercase), 0 (otherwise)
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
