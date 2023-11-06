2d42ce43febb87256cfa3990504b2bb2b3f2724a
RaquelCos <raquelcostelha@gmail.com>
2023-11-06 05:35:57 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two int
+ * @a: Pointer to first value
+ * b: Pointer to Second Value
+ */
+void swap_int(int *a, int *b)
+{
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
