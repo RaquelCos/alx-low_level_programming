282ca18d3c8aa4cc9c400934f1f83e432ba93df7
RaquelCos <raquelcostelha@gmail.com>
2023-11-13 04:28:12 -0800

    Task 4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_strspn - Gets the length of a prefix substr
+ * @s: String evaluated
+ * @accept: String containing the list of chars to match
+ *
+ * Return: Number of bytes in intital segment of s
+ * which consist only of bytes from accpet
+ */
+unsigned int _strspn(char *s, char *accept);
+{
+	int i, j, f, flag;
+
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accpet[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
+		}
+	}
+
+	return (0);
+}
